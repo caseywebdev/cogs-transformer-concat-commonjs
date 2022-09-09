e3bec57e8a1d9a6519f72ea0fb8e83124a03569a
Casey Foster <c@sey.me>
2022-09-09 10:16:47 -0500

    Add injection-time error handling, 3.5.12

--- test/output.js
+++ test/output.js
@@ -1,8 +1,8 @@
 var Cogs = (function () {
   'use strict';
 
+  var asyncs = {};
   var fetches = {};
-  var modulePromises = {};
   var modules = {};
 
   var define = function (path, factory) {
@@ -31,49 +31,66 @@ var Cogs = (function () {
     return module.exports;
   };
 
+  window.addEventListener('error', function (event) {
+    var deferred = fetches[event.filename];
+    if (deferred && deferred.status === 'pending') deferred.reject(event.error);
+  });
+
+  var createDeferred = function () {
+    var deferred = { status: 'pending' };
+    deferred.promise = new Promise(function (_resolve, _reject) {
+      deferred.resolve = function (value) {
+        if (deferred.status === 'pending') {
+          deferred.status = 'fulfilled';
+          _resolve(value);
+        }
+      };
+      deferred.reject = function (value) {
+        if (deferred.status === 'pending') {
+          deferred.status = 'rejected';
+          _reject(value);
+        }
+      };
+    });
+    return deferred;
+  };
+
   var fetch = function (src) {
-    return (
-      fetches[src] ||
-      (fetches[src] = new Promise(function (resolve, reject) {
-        var script = document.createElement('script');
-        script.async = true;
-        script.src = src;
-        script.addEventListener('load', function () {
-          try {
-            resolve();
-          } catch (error) {
-            reject(error);
-          }
-        });
-        script.addEventListener('error', function () {
-          reject(new Error("Failed to load '" + src + "'"));
-        });
-        document.head.appendChild(script);
-      }).catch(function (error) {
-        delete fetches[src];
-        throw error;
-      }))
-    );
+    var script = document.createElement('script');
+    script.src = src;
+    var deferred = fetches[script.src];
+    if (deferred && deferred.status !== 'rejected') return deferred.promise;
+
+    deferred = fetches[script.src] = createDeferred();
+    script.addEventListener('load', deferred.resolve);
+    script.addEventListener('error', function () {
+      deferred.reject(new Error("Failed to load '" + src + "'"));
+    });
+    document.head.appendChild(script);
+    return deferred.promise;
   };
 
   require.async = function (path, manifest) {
-    return (
-      modulePromises[path] ||
-      (modulePromises[path] = new Promise(function (resolve, reject) {
-        if (modules[path]) return resolve(require(path));
+    var deferred = asyncs[path];
+    if (deferred && deferred.status !== 'rejected') return deferred.promise;
 
-        var srcs = manifest == null ? path : manifest[path];
-        if (!Array.isArray(srcs)) srcs = [srcs];
-        Promise.all(srcs.map(fetch))
-          .then(function () {
-            return resolve(require(path));
-          })
-          .catch(reject);
-      }).catch(function (error) {
-        delete modulePromises[path];
-        throw error;
-      }))
-    );
+    deferred = asyncs[path] = createDeferred();
+    if (modules[path]) {
+      try {
+        deferred.resolve(require(path));
+      } catch (error) {
+        deferred.reject(error);
+      }
+    } else {
+      var srcs = manifest == null ? path : manifest[path];
+      if (!Array.isArray(srcs)) srcs = [srcs];
+      Promise.all(srcs.map(fetch))
+        .then(function () {
+          return deferred.resolve(require(path));
+        })
+        .catch(deferred.reject);
+    }
+    return deferred.promise;
   };
 
   return { define: define, modules: modules, require: require };
